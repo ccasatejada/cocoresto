88b8a040e42cbff2422101040a6cbfe46346f067
cdi104 <cdi104@pltz836.AFPA-PARIS-DI>
2015-08-24 14:06:27 +0200

    Merge origin/master
    Conflicts:
    	cocoresto-war/web/WEB-INF/web.xml

--- cocoresto-war/src/java/controllers/EmployeeController.java
+++ cocoresto-war/src/java/controllers/EmployeeController.java
@@ -1,7 +1,23 @@
 
 package controllers;
 
-public class EmployeeController {
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EmployeeController implements IController{
+
+    
+    
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String execute(HttpServlet servlet, HttpServletRequest request, HttpServletResponse response) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
     
