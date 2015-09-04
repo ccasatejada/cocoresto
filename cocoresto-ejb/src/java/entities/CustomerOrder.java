62128d36b39cbdca8413687ae15ed36f692f633a
cdi110 <cdi110@pltz843.AFPA-PARIS-DI>
2015-09-04 09:43:42 +0200

    Update customerOrder datetime

--- cocoresto-ejb/src/java/entities/CustomerOrder.java
+++ cocoresto-ejb/src/java/entities/CustomerOrder.java
@@ -14,7 +14,7 @@ public class CustomerOrder implements Serializable {
     private Long id;
     @Column(nullable = false, unique = true)
     private String number;
-    @Temporal(javax.persistence.TemporalType.DATE)
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
     @Column(nullable = false)
     private Date orderDate;
     @Column(nullable = false)
