3907bf1a02262f353191959f9d079e056ea05460
cdi110 <cdi110@pltz843.AFPA-PARIS-DI>
2015-09-08 16:30:41 +0200

    Update dashboard waiter : order orders

--- cocoresto-ejb/src/java/ejb/ejbCustomerOrder.java
+++ cocoresto-ejb/src/java/ejb/ejbCustomerOrder.java
@@ -58,7 +58,7 @@ public class ejbCustomerOrder implements ejbCustomerOrderLocal {
 
     @Override
     public List<CustomerOrder> findAllByRangeByEmployee(int firstResult, int maxResults, Long id) {
-        Query q = em.createQuery("select co from CustomerOrder co where co.active = 1 AND co.employee.id = " + id + " order by co.orderDate desc");
+        Query q = em.createQuery("select co from CustomerOrder co where co.active = 1 AND co.employee.id = " + id + " order by co.status desc, co.orderDate desc");
         if (firstResult >= 0) {
             q.setFirstResult(firstResult);
         }
