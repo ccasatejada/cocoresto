3e96f255713efcf0d023e5d91fbd089622549bb5
cdi110 <cdi110@pltz843.AFPA-PARIS-DI>
2015-09-09 15:01:56 +0200

    Update Ajax

--- cocoresto-war/src/java/helpers/Ajax.java
+++ cocoresto-war/src/java/helpers/Ajax.java
@@ -1,6 +1,7 @@
 package helpers;
 
 import ejb.ejbRestaurantLocal;
+import entities.Category;
 import entities.CustomerTable;
 import entities.Employee;
 import java.io.IOException;
@@ -13,16 +14,55 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import models.beanCategory;
+import models.beanDish;
 import models.beanTableCustomer;
 
 @WebServlet(name = "Ajax", urlPatterns = {"/Ajax"})
 public class Ajax extends HttpServlet {
+
     @EJB
     private ejbRestaurantLocal ejbRestaurant;
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
+
+            if ("menu".equals(request.getParameter("task"))) {
+                
+                beanCategory bc = new beanCategory();
+                String type = "Plat";
+
+                if ("dishes".equals(request.getParameter("list"))) {
+                    type = "Plat";
+                }
+
+                if ("drinks".equals(request.getParameter("list"))) {
+                    type = "Boisson";
+                }
+
+                if ("combos".equals(request.getParameter("list"))) {
+                    type = "Menu";
+                }
+
+                List<Category> categories = bc.findAvailableCategories(type);
+
+                out.println("<div id=\"listMenu\" class=\"panel-group\" role=\"tablist\" aria-multiselectable=\"true\">");
+                for (Category cat : categories) {
+                    out.println("<div class=\"panel panel-default\">\n"
+                            + "<div class=\"panel-heading\" role=\"tab\" id=\"heading" + cat.getId() + "\">\n"
+                            + "<h4 class=\"panel-title\">\n"
+                            + "<a role=\"button\" data-toggle=\"collapse\" data-parent=\"#listMenu\" href=\"#collapse" + cat.getId() + "\" aria-expanded=\"false\" aria-controls=\"collapse" + cat.getId() + "\">" + cat.getName() + "</a>\n"
+                            + "</h4>\n"
+                            + "</div>\n"
+                            + "<div id=\"collapse" + cat.getId() + "\" class=\"panel-collapse collapse\" role=\"tabpanel\" aria-labelledby=\"heading" + cat.getId() + "\">\n"
+                            + "<div class=\"panel-body\">");
+
+                    out.println("</div>\n</div>\n</div>");
+                }
+                out.println("</div>");
+
+            }
 
             if ("customerTable".equals(request.getParameter("task"))) {
                 beanTableCustomer btc = new beanTableCustomer();
